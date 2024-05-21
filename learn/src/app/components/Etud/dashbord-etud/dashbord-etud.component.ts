75a36d1639242bf15987c7f5ee9860a6cd6545d5
Hanine Benali <115839915+Imen-Ht07@users.noreply.github.com>
2024-05-22 00:38:27 +0200

    Notification

--- learn/src/app/components/Etud/dashbord-etud/dashbord-etud.component.ts
+++ learn/src/app/components/Etud/dashbord-etud/dashbord-etud.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { NotificationService } from 'src/app/services/notification.service';
 @Component({
   selector: 'app-dashbord-etud',
   templateUrl: './dashbord-etud.component.html',
   styleUrls: ['./dashbord-etud.component.css']
 })
-export class DashbordEtudComponent {
+export class DashbordEtudComponent implements OnInit {
+  notifications: any[] = [];
 
+  constructor(private notificationService: NotificationService) {}
+
+  ngOnInit() {
+    this.notificationService.getAllNotifications().subscribe({
+      next: (data) => {
+        this.notifications = data;
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      }
+    });
+  }
 }
